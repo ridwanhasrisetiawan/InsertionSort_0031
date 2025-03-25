bd64eb244cb5dc48790d5ca7d1977351d32aef5f
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-03-26 02:19:47 +0800

    insertionsort4_5

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -40,6 +40,16 @@ void insertionSort()
 
     for (i = 1; i <= n -1; i++)
 {
+
+    temp = arr[i];
+
+    j = i - 1;
+
+    while (j >= 0 && arr[j] > temp)
+    {
+        arr[j + 1] = arr[j];
+        j--;
+    }
     
 }
 }
