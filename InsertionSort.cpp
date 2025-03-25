e906b5e6a051628eb4184ca8d49bb24ab6e3173a
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-03-26 02:29:04 +0800

    insertionsort4_7

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -63,5 +63,19 @@ void display()
     cout << "===============================" << endl;
     cout << "Element Array yang telah tersusun" << endl;
     cout << "===============================" << endl;
-    
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    insertionSort();
+    display();
+    system("pause");
+    return 0;
 }
