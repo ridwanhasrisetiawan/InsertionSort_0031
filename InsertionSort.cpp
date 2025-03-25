ef1e37f69f9ce794c0c45c25baa7794d099bce9d
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-03-26 02:15:55 +0800

    insertionsort4_4

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -24,4 +24,22 @@ void input()
     cout << "======================" << endl;
     cout << "Masukkan Element Array" << endl;
     cout << "======================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+void insertionSort()
+{
+
+    int temp;
+    int j, i;
+
+    for (i = 1; i <= n -1; i++)
+{
+    
+}
 }
